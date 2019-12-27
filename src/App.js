19900e74de4a8da2d4eb3a097b9f43f15ea42c51
PGF <jamil123ff@gmail.com>
2019-12-27 19:22:46 +0200

    adding test page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home   from './component/Home'
-import About from './component/Aboutus'
-import Contactus  from './component/Contactus'
-import Signin   from './component/Sign'
-import Login   from './component/Login'
+import Home   from './component/Home';
+import About from './component/Aboutus';
+import Contactus  from './component/Contactus';
+import Signin   from './component/Sign';
+import Login   from './component/Login';
+import Test from './component/test';
 
 
 class App extends Component{
   render(){
     return(
       <div>
-     <Home></Home>
-     <About></About>
-     <Contactus></Contactus>
+        <Home></Home>
+        <About></About>
+        <Contactus></Contactus>
+        <Test></Test>
+
        <Login></Login>
        <Signin></Signin>
    
