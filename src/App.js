c07425eeb8f0abfdffe52e230ab7e197ba929249
PGF <jamil123ff@gmail.com>
2019-12-28 12:36:04 +0200

    edits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route,Switch}from "react-router-dom"
 
 
 import './App.css';
-import Nav from './component/Nav'
+import Nav from './component/nav'
 import Home   from './component/Home';
 import Aboutus from './component/Aboutus';
 import Contactus  from './component/Contactus';
@@ -10,8 +11,6 @@ import Signin   from './component/Sign';
 import Login   from './component/Login';
 
 
-import { BrowserRouter, Route,Switch}from "react-router-dom"
-import Nav from './component/nav';
 
 
 
@@ -26,7 +25,7 @@ class App extends Component{
      <Nav></Nav>
     <Switch>
     <Route path="/" component={Home} exact></Route>
-    <Route path="/about"component={About}></Route>
+    <Route path="/about"component={Aboutus}></Route>
     <Route path="/contact"component={Contactus}></Route>
     <Route path="/sign"component={Signin}></Route>
     <Route path="/login"component={Login}></Route>
