c9152b32b6f9aa438d81ea6acb411314032fbcbb
PGF <jamil123ff@gmail.com>
2019-12-28 17:35:52 +0200

    edit fillin file

--- src/component/fillin.js
+++ src/component/fillin.js
@@ -1,16 +1,42 @@
 
+
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 
 
 class Fillin extends Component{
   render(){
     return(
-      <div style={{backgroundColor:'red' }}>
-        
-<p>dhfjsdgfhjgsdjhfghj</p>
-        <img src={{uri:"https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"}} />
-</div>
+      <div>
+      <div>
+      <h1> استمارة المريض  </h1>
+
+
+<h3>  الإسم الكامل للمريض </h3>
+<TextField id="filled-basic" label="اسم المريض رباعي" variant="filled" />
+
+<h3>  أمراض مزمنة أو أمراض أخرى </h3>
+<TextField id="filled-basic" label="أمراض أخرى" variant="filled" />
+
+<h3>  رقم هاتف المريض </h3>
+<TextField id="filled-basic" label="رقم الهاتف" variant="filled" />
+
+<h3> ملاحظات إضافية  </h3>
+<TextField id="filled-basic" label="" variant="filled" />
+
+
+
+        </div>
+        <div>
+        <Button variant="contained" color="primary"className="submitbutton"> ارسال </Button>
+        </div>
+
+
+
+      </div>
+
 
     )
   }
