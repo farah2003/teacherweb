e1113716286624bdee6bcf65da1d0c7ab8b7b476
farah2003 <farah.shaqoura1232003@gmail.com>
2019-12-30 21:53:26 +0200

    add For patient

--- src/component/fillin.js
+++ src/component/fillin.js
@@ -3,41 +3,41 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import './style/fill.css'
 import SNav from './secandNav'
 
 
 class Fillin extends Component{
   render(){
     return(
-      <div>
-          <SNav></SNav>
-      <div>
-      <h1> استمارة المريض  </h1>
+      <div className="div">
+          <SNav className="header"></SNav>
+          <div  className="body" >
+                 <div  >
+           
+               <p className="h1"> استمارة المريض  </p>
 
 
-<h3>  الإسم الكامل للمريض </h3>
-<TextField id="filled-basic" label="اسم المريض رباعي" variant="filled" />
+              <h3 className="h3">  الإسم الكامل للمريض </h3>
+              <TextField id="filled-basic" label="اسم المريض رباعي" variant="filled" />
 
-<h3>  أمراض مزمنة أو أمراض أخرى </h3>
-<TextField id="filled-basic" label="أمراض أخرى" variant="filled" />
+              <h3  className="h3">  أمراض مزمنة أو أمراض أخرى </h3>
+              <TextField id="filled-basic" label="أمراض أخرى" variant="filled" />
 
-<h3>  رقم هاتف المريض </h3>
-<TextField id="filled-basic" label="رقم الهاتف" variant="filled" />
+              <h3  className="h3">  رقم هاتف المريض </h3>
+              <TextField id="filled-basic" label="رقم الهاتف" variant="filled" />
 
-<h3> ملاحظات إضافية  </h3>
-<TextField id="filled-basic" label="" variant="filled" />
+              <h3  className="h3"> ملاحظات إضافية  </h3>
+              <TextField id="filled-basic" label="" variant="filled" />
+              </div>
+               <div className="button">
+          <Button variant="contained" color="primary"className="submitbutton"> ارسال </Button>
+          </div>
 
-
+          
 
         </div>
-        <div>
-        <Button variant="contained" color="primary"className="submitbutton"> ارسال </Button>
         </div>
-
-
-
-      </div>
 
 
     )
