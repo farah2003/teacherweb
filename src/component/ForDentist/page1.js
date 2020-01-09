c0fe6a1f84b4eb5863132fb2440a760a36ff6e52
karamhillis <jamil123ff@gmail.com>
2020-01-09 15:15:01 +0300

    editprof

--- src/component/ForDentist/page1.js
+++ src/component/ForDentist/page1.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import DNav from './navForDent'
 
-import DHome from './HomeDent'
-import Mainfordent from './main'
-import { Card } from 'antd';
+import { Card , Icon, Avatar } from 'antd';
 
 const { Meta } = Card;
 class  FiratTab extends Component{
     render()
     {return(
      
-        <div style={{backgroundColor:'red'}}>
+        <div style={{backgroundColor:'#34495e', flex:1}}>
             
          
-            <DHome {...this.props}>  </DHome>
-            <div style={{marginTop:120}}>
-            <Card  title={<h4 style={{height:10,marginTop:3,fontWeight:'bold' ,marginRight:30}}>farah shaqoura</h4> }  style={{ width: 900,height:200 ,marginLeft:220,marginBottom:40, marginTop:10}}>
-            <label>16</label><h4>:االعمر</h4><label>16</label>
-            </Card>
+            <div>
+                
+            <Card
+    style={{ width: 300, marginLeft:520 }}
+    cover={
+      <img
+        alt="example"
+        src="https://www.eafhc.org/wp-content/uploads/sites/411/2019/02/content-image-dental.png"
+      />
+    }
+    actions={[
+      <a href="https://www.instagram.com/kemor1/"><Icon type="instagram" /></a>,
+      <a href="https://www.facebook.com/karam.hillis"><Icon type="facebook" /></a>,
+      <a href="https://twitter.com/karam1st"><Icon type="twitter"/></a>,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://png.pngtree.com/png-clipart/20190115/ourmid/pngtree-love-tooth-day-female-dentist-dentist-in-white-coat-long-hair-png-image_344681.jpg" />}
+      title="D. Amal"
+      description="Dental dentist student in his 4th year at college."
+    />
+  </Card>,
+  <Card size="Large" title="Information" extra={<a href="#">More</a>} style={{ width: 1350 }}>
+      <h1 style={{fontFamily:'Helvetica Neue'}}>Name:<h1 style={{fontFamily:'Segoe UI Emoji',fontSize:20}}>Amal Mohammed</h1></h1>
+      
+      <label style={{marginBottom:200}}>
+      <h1 style={{fontFamily:'Helvetica Neue'}}>Date of Birth:<h1 style={{fontFamily:'Segoe UI Emoji',fontSize:20}}>13/6/2003</h1></h1>
+      </label>
+      <label style={{marginBottom:200}}>
+      <h1 style={{fontFamily:'Helvetica Neue'}}>Phone Number:<h1 style={{fontFamily:'Segoe UI Emoji',fontSize:20}}>021319312</h1></h1>
+      </label>
+    </Card>
+
+ 
+  
           
         </div>
+        
+     
+
+         
         </div>
+
   
         )
     }
